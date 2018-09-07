d82fc7173b1830e09145873da31543a48c147a66
User <User@DESKTOP-76HRC5D>
2018-09-07 22:32:06 +0530

    sign in login

---
+++ DB.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function getDBConnection() {
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "123";
+    $db = "hmsdb";
+
+
+// Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+// Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        return $conn;
+    }
+}
+
+
+function setData($sql, $msgArray) {
+    $conn = getDBConnection();
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    if (mysqli_query($conn, $sql)) {
+        $last_id = mysqli_insert_id($conn);
+        if ($last_id>0)
+            echo '<p class="bg-success msg-success">'.$msgArray['msgsuccess'].'<p>';
+    } else {
+        echo '<p class="bg-danger msg-error">'.$msgArray['msgerror'].'<p>';
+    }
+
+    mysqli_close($conn);
+}
+
+function getData($sql) {
+    // Create connection
+    $conn = getDBConnection();
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        return $result;
+    } else {
+        return FALSE;
+    }
+
+    mysqli_close($conn);
+}
+
+function setUpdate($sql, $MSG) {
+    $flag = TRUE;
+    $conn = getDBConnection();
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    if (mysqli_query($conn, $sql)) {
+        if ($MSG)
+            echo '<p class="bg-success msg-success">Record updated successfully<p>';
+    } else {
+        if ($MSG)
+            echo "Invalid Input";
+        $flag = FALSE;
+    }
+
+    mysqli_close($conn);
+    return $flag;
+}
+
+function setDelete($sql) {
+    $flag = TRUE;
+    $conn = getDBConnection();
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    if (mysqli_query($conn, $sql)) {
+        //echo "Record deleted successfully";
+    } else {
+        // echo "Error deleting record: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+
+
+?>
